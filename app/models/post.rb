b72bb91c349a63e6835fa56b398194581a3f1c91
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-08-30 07:48:46 +0300

    commit tree aa5e53f6e5f6625a3ce275c60e35bc4b0bb373ba

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    # validate :choose_category
+
+    private
+    # def choose_category
+    #    return if self.category == "Fiction" || self.category == "Non-Fiction"           
+    #     end
+    # end
 end
